fe25829789052d2445729723439791e8419b0636
agus@mumuki.org <agus@mumuki.org>
2020-11-27 14:25:33 +0000

    Mumuki Export on 2020-11-27 14:25:33 +0000

--- 00001_¿Dónde juega_/test..js
+++ 00001_¿Dónde juega_/test..js
@@ -1,9 +1,9 @@
 describe("", function() {
-  it("Riquelme juega en boca", function () => {
+  it("Riquelme juega en boca", function () {
     const equipo = await obtenerEquipo('Riquelme');
     assert(equipo, 'Boca')
   })
-  it("Milito juega en Racing", function () => {
+  it("Milito juega en Racing", function () {
     const equipo = await obtenerEquipo('Milito');
     assert(equipo, 'Racing')
   })
