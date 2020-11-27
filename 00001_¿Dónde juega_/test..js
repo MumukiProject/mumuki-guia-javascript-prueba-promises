5b29c46aa9327fbba32bbc856887cccf1b92fce7
gastonprietoutn@gmail.com <gastonprietoutn@gmail.com>
2020-11-27 17:49:58 +0000

    Mumuki Export on 2020-11-27 17:49:58 +0000

--- 00001_¿Dónde juega_/test..js
+++ 00001_¿Dónde juega_/test..js
@@ -1,12 +1,12 @@
 describe("", function() {
   it("Riquelme juega en boca", () => {
     return obtenerEquipo('Riquelme').then(function(equipo) {
-      assert.equals(equipo, 'Boca')  
+      assert(equipo, 'Boca')  
     });
   })
   it("Milito juega en Racing", () => {
     return obtenerEquipo('Milito').then(function(equipo) {
-      assert.equals(equipo, 'Racing')  
+      assert(equipo, 'Racing')  
     });
     
   })
